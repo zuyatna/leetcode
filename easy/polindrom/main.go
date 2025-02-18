c1afe92b7bbc9742da3a467313764e4d7351559b
zuyatna <zuyatna@gmail.com>
2025-02-18 12:26:20 +0700

    add slow and fast

--- easy/polindrom/main.go
+++ easy/polindrom/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	// fast
 	fmt.Println(isPolindrom(121))
 	fmt.Println(isPolindrom(-121))
+
+	// slow
+	fmt.Println(isPolindrom2(121))
+	fmt.Println(isPolindrom2(-121))
 }
 
 func isPolindrom(x int) bool {
@@ -21,4 +29,15 @@ func isPolindrom(x int) bool {
 	}
 
 	return original == reversed
+}
+
+func isPolindrom2(x int) bool {
+	str := strconv.Itoa(x)
+
+	for i := 0; i < len(str)/2; i++ {
+		if str[i] != str[len(str)-1-i] {
+			return false
+		}
+	}
+	return true
 }
---
+++ easy/two-sum/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fast
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+
+	// slow
+	fmt.Println(twoSum2([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
